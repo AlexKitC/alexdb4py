dd5bbb389133e628edd82c66b613ae7562b214da
alex <392999164@qq.com>
2023-06-22 22:23:20 +0800

    feat: finish new connection conf page

--- src/main/java/org/alex/db/controller/HomeController.java
+++ src/main/java/org/alex/db/controller/HomeController.java
@@ -1,12 +1,19 @@
 package org.alex.db.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.stage.Stage;
 import org.alex.db.entity.ConnItem;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 /**
@@ -18,6 +25,10 @@ public class HomeController implements Initializable {
 
     @FXML
     private TreeView<ConnItem> connItemTreeView;
+    @FXML
+    private Button newConnButton;
+    @FXML
+    private Button newQueryButton;
 
 
     @Override
@@ -28,5 +39,21 @@ public class HomeController implements Initializable {
         rootItem.setExpanded(true);
         rootItem.getChildren().add(new TreeItem<>());
         connItemTreeView = new TreeView<>(rootItem);
+    }
+
+
+    @FXML
+    private void onClickNewConnButton() throws IOException {
+        Stage newConnStage = new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/new-conn.fxml")));
+        Scene scene = new Scene(root);
+        newConnStage.setTitle("新建连接");
+        newConnStage.setScene(scene);
+        newConnStage.show();
+    }
+
+    @FXML
+    private void onClickNewQueryButton() {
+        System.out.println("cli");
     }
 }
---
+++ src/main/java/org/alex/db/controller/NewConnController.java
@@ -0,0 +1,48 @@
+package org.alex.db.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+/**
+ * @author alex
+ * @version 1.0.0
+ * @since 2023/6/22 21:34
+ */
+public class NewConnController {
+
+    @FXML
+    private Button newConnTestConnButton;
+    @FXML
+    private Button newConnCancelButton;
+    @FXML
+    private Button newConnSaveButton;
+    @FXML
+    private TextField newConnNameTextField;
+    @FXML
+    private TextField newConnHostTextField;
+    @FXML
+    private TextField newConnPortTextField;
+    @FXML
+    private TextField newConnUsernameTextField;
+    @FXML
+    private TextField newConnPasswordTextField;
+    @FXML
+    private CheckBox newConnRememberPasswordCheckbox;
+
+    @FXML
+    private void onClickTestNewConn() {
+
+    }
+
+    @FXML
+    private void onClickCancelNewConn() {
+
+    }
+
+    @FXML
+    private void onClickSaveNewConn() {
+
+    }
+}
